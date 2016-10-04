08f114cfc4012b98107176a2aab7e982270e48c2
apeltzer <alex.peltzer@mailbox.org>
2016-10-04 12:05:06 +0200

    Updated including PKGbuilds and gradle changes

---
+++ arch-pkgbuild/starterGUI.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+VERSION=1.92.17
+java -jar /opt/eager-gui/EAGER-GUI-$VERSION.jar $*
---
+++ arch-pkgbuild/updatePKGBuild.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Replace version information appropriately in PKGBUILD and starterScript
+VERSION=$(grep -e '^version' ../build.gradle | tr -d "'" | tr -d "version ")
+VERSIONREL=1
+#PKGBUILD
+sed -i "s/^pkgver=.*/pkgver=$VERSION/" PKGBUILD
+sed -i "s/^pkgrel=.*/pkgrel=$VERSIONREL/" PKGBUILD
+#startscript
+sed -i "s/^VERSION=.*/VERSION=$VERSION/" starterGUI.sh
+
+cp ../build/libs/*.jar . 
+makepkg
