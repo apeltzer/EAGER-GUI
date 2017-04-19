986afee8ccf21ce3a4a05f8b9a21c5a68d054093
peltzer <alexander.peltzer@uni-tuebingen.de>
2017-04-19 12:35:00 +0200

    EAGER v 1.92.30 can now use AdapterRemoval for CLipping and Merging of reads.

--- src/main/java/main/AdapterRemovalDialog.java
+++ src/main/java/main/AdapterRemovalDialog.java
@@ -9,15 +9,22 @@ public class AdapterRemovalDialog extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
+    private JTextField minimum_base_quality_textfield;
+    private JTextField minimum_sequence_length_textfield;
+    private JTextField minimum_adapter_overlap_textfield;
+    private JCheckBox performOnlyAdapterClippingCheckBox;
+    private JTextField forward_adapter_textfield;
+    private JTextField reverse_adapter_textfield;
 
     public AdapterRemovalDialog(Communicator communicator) {
+        setValues(communicator);
         setContentPane(contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
 
         buttonOK.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                onOK();
+                onOK(communicator);
             }
         });
 
@@ -43,8 +50,13 @@ public class AdapterRemovalDialog extends JDialog {
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
 
-    private void onOK() {
-        // add your code here
+    private void onOK(Communicator c) {
+        c.setMerge_fwadaptor(forward_adapter_textfield.getText());
+        c.setMerge_bwadaptor(reverse_adapter_textfield.getText());
+        c.setQuality_minreadquality(Integer.parseInt(minimum_base_quality_textfield.getText()));
+        c.setQuality_readlength(Integer.parseInt(minimum_sequence_length_textfield.getText()));
+        c.setMerge_only_clipping(performOnlyAdapterClippingCheckBox.isSelected());
+        c.setMerge_min_adapter_overlap(Integer.parseInt(minimum_adapter_overlap_textfield.getText()));
         dispose();
     }
 
@@ -53,10 +65,24 @@ public class AdapterRemovalDialog extends JDialog {
         dispose();
     }
 
-    public static void main(String[] args) {
-        AdapterRemovalDialog dialog = new AdapterRemovalDialog();
-        dialog.pack();
-        dialog.setVisible(true);
-        System.exit(0);
+    private void setValues(Communicator c){
+        if(c.getMerge_fwadaptor() != null) {
+            this.forward_adapter_textfield.setText(c.getMerge_fwadaptor());
+        }
+        if(c.getMerge_bwadaptor() != null){
+            this.reverse_adapter_textfield.setText(c.getMerge_bwadaptor());
+        }
+        if (c.getQuality_minreadquality() == 0) {
+            this.minimum_base_quality_textfield.setText(String.valueOf(c.getQuality_minreadquality()));
+        }
+        if (c.getQuality_readlength() == 0) {
+            this.minimum_sequence_length_textfield.setText(String.valueOf(c.getQuality_readlength()));
+        }
+        if (c.isMerge_only_clipping()) {
+            this.performOnlyAdapterClippingCheckBox.setSelected(c.isMerge_only_clipping());
+        }
+        if(c.getMerge_min_adapter_overlap() == 0){
+            this.minimum_adapter_overlap_textfield.setText(String.valueOf(c.getMerge_min_adapter_overlap()));
+        }
     }
 }
